c6ceedb844813e14b6a7b2acbe84da4fec90bb92
mansouri <mansouri@email.arizona.edu>
2018-05-18 21:54:01 -0700

    scoreBoard fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,6 @@ var lettersGuessed = [];
 var userChoice = [];
 
 
-
 //letter computer can pick
 var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 console.log(letters);
@@ -26,13 +25,14 @@ document.addEventListener('keypress', (event) => {
 
 })
 
+
 //changes wins and losses 
 function compare(userChoice, computerChoice){
     if (userChoice === computerChoice) {
         console.log("You won");
         wins++;
         document.getElementById('wins').innerHTML = 'Wins: ' + wins;
-        guessesLeft = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9","10");
+        guessesLeft = ("0","1", "2", "3", "4", "5", "6", "7", "8", "9","10");
         lettersGuessed = [];
         guessesLeft = 10;
        
@@ -43,17 +43,11 @@ function compare(userChoice, computerChoice){
         document.getElementById('losses').innerHTML = 'losses: ' + losses;
         guessesLeft = 10;
         lettersGuessed = [];
-       
-        
+    
     }else if (userChoice !== computerChoice) {
         console.log("lost a letter");
         guessesLeft--;
-            if (guessesLeft === 0){
-                guessesLeft = 10;
-                lettersGuessed = [];
-            }
         document.getElementById('guessesLeft').innerHTML = 'Guesses Left: ' + guessesLeft;   
         document.getElementById('lettersGuessed').innerHTML = 'Letters Guessed: ' + lettersGuessed;
-        
     }
 }
