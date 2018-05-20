8ab33c858df217e81699dba16dd776f9aa89ba9b
mansouri <mansouri@email.arizona.edu>
2018-05-19 19:10:49 -0700

    The random letter changes aafter each round

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Psychic-Game
+### This is a letter guessing game. You have 10 chances to pick the random  letter that was chosen by the computer.###
+### If you fail to guess the letter you will lose and a new letter will be picked for the next round###
+### The score will be kept on the scoreboard until the page is refreshed###
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,54 @@
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
-var lettersGuessed = [];
-var userChoice = [];
+var guesses = 10;
+var usersLetterChoices = [];
 
-
-//letter computer can pick
-var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-console.log(letters);
-
-
-//computer picking random letter
-var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-console.log("computer choice" , computerChoice);
-
+//picks a random letter to be guessed
+function computerChoiceFunction() {
+    var letters = ["a", "b", "c", "d","e", "f","g", "h","i", "j","k", "l","m", "n","o", "p","q", "r","s", "t","u", "v","w", "x","y","z"];
+    
+    var rletters = letters[Math.floor(Math.random() * letters.length)];
+    return rletters
+    console.log(rletters);
+}
+// sets computers choice equal to the random letter picked
+var computerChoice = computerChoiceFunction();
+console.log(computerChoiceFunction);
 
 // takes in users choice and keeps a log 
 document.addEventListener('keypress', (event) => {
     var userChoice = event.key;
     console.log(userChoice);
-    lettersGuessed.push(userChoice);
+    usersLetterChoices.push(userChoice + " ");
     compare(userChoice , computerChoice);
-    var userChoice = lettersGuessed;
+    document.getElementById('lettersGuessed').innerHTML = 'Letters Guessed: ' + usersLetterChoices;
 
 })
-
-
-//changes wins and losses 
-function compare(userChoice, computerChoice){
-    if (userChoice === computerChoice) {
-        console.log("You won");
-        wins++;
-        document.getElementById('wins').innerHTML = 'Wins: ' + wins;
-        guessesLeft = ("0","1", "2", "3", "4", "5", "6", "7", "8", "9","10");
-        lettersGuessed = [];
-        guessesLeft = 10;
-       reset();
-
-    }else if ((guessesLeft === 0) || (lettersGuessed.lenngth === 10)){
-        losses++;
-        console.log("You lost")
-        document.getElementById('losses').innerHTML = 'losses: ' + losses;
-        guessesLeft = 10;
-        lettersGuessed = [];
-        reset();
+//resets everything excep the score of the game
+function reset() {
+    computerChoice = computerChoiceFunction();
+    guesses = 10;
+    document.getElementById('wins').innerHTML = 'Wins: ' + wins;
+    document.getElementById('losses').innerHTML = 'losses: ' + losses;
     
-    }else if (userChoice !== computerChoice) {
-        console.log("lost a letter");
-        guessesLeft--;
-        document.getElementById('guessesLeft').innerHTML = 'Guesses Left: ' + guessesLeft;   
-        document.getElementById('lettersGuessed').innerHTML = 'Letters Guessed: ' + lettersGuessed;
+    console.log ("computers choice is " + computerChoice);
+}
 
-    }
-    function reset() {
-        var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-        console.log("computer choice" , computerChoice);
+function compare(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        wins++;
+        usersLetterChoices = [];
+        reset();
 
+    }else if ( guesses === 0) {
+        losses++;
+        usersLetterChoices = [];
+        reset();
+
+    }else if (userChoice != computerChoice) {
+        guesses--;
+        document.getElementById('guessesLeft').innerHTML = 'Guesses Left: ' + guesses;   
     }
 }
 
+reset();
